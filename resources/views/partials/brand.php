26c1a6d60e0a74b4c0951416b045bfe56357e753
johnwickfuo <64735307+johnwickfuo@users.noreply.github.com>
2026-09-25 18:12:43 +0100

    Replace legacy brand mark with ApexTrades logo

--- resources/views/partials/brand.php
+++ resources/views/partials/brand.php
@@ -1,6 +1,12 @@
-<a class="brand brand-premium notranslate" translate="no" href="<?= e(route('home')) ?>" aria-label="Upgraded Broker">
+<?php
+$brandName='ApexTrades';
+?>
+<a class="brand brand-premium notranslate" translate="no" href="<?= e(route('home')) ?>" aria-label="<?= e($brandName) ?>">
   <span class="brand-emblem" aria-hidden="true">
-    <svg viewBox="0 0 44 44" focusable="false"><path d="M8 11v13.5C8 33 13.8 38 22 38s14-5 14-13.5V11h-6v13.2c0 5-2.9 7.7-8 7.7s-8-2.7-8-7.7V11H8Z"/><path d="M18 6h18v6H18z"/></svg>
+    <svg viewBox="0 0 44 44" focusable="false" role="img">
+      <path d="M5.5 34 19.5 8h6.8l12.2 26h-7.2l-3-6.8H16.8L13 34H5.5Zm14-12.8h6.1l-2.8-6.5-3.3 6.5Z"/>
+      <path d="M28.5 8H39v10.5h-4.5v-3.2l-7.1 7.1-3.2-3.2 7.1-7.1h-2.8V8Z"/>
+    </svg>
   </span>
-  <span class="brand-words"><strong>UPGRADED</strong><small>BROKER</small></span>
+  <span class="brand-words"><strong>APEX</strong><small>TRADES</small></span>
 </a>
