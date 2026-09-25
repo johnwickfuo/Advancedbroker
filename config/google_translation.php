613c93c6a15ae2de8a186258ebb97aaaab546e6c
johnwickfuo <64735307+johnwickfuo@users.noreply.github.com>
2026-09-25 21:26:50 +0100

    Remove obsolete Google Cloud translation file config/google_translation.php

--- config/google_translation.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'enabled'=>filter_var(env('GOOGLE_TRANSLATION_ENABLED',true),FILTER_VALIDATE_BOOL),
-    'api_key'=>(string)env('GOOGLE_TRANSLATE_API_KEY',''),
-    'cache_seconds'=>(int)env('GOOGLE_TRANSLATION_CACHE_SECONDS',604800),
-    'timeout_seconds'=>max(2,min(15,(int)env('GOOGLE_TRANSLATION_TIMEOUT_SECONDS',6))),
-];
