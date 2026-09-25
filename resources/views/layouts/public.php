10f6aa64ee0ec70bac0516016f4dc51df431c150
johnwickfuo <64735307+johnwickfuo@users.noreply.github.com>
2026-09-25 19:16:44 +0100

    Inline Smartsupp directly in public.php

--- resources/views/layouts/public.php
+++ resources/views/layouts/public.php
@@ -16,7 +16,18 @@ $currentUser=!empty($_SESSION['user_id'])?app('users')->find((int)$_SESSION['use
 <title><?= e(($title??'').' · '.($branding['brand_name']??config('app.name'))) ?></title>
 <link rel="stylesheet" href="<?= e(asset('css/app.css')) ?>">
 <style nonce="<?= e(app('theme')->nonce()) ?>"><?= app('theme')->css($context->theme()) ?></style>
-<?php require __DIR__.'/../partials/smartsupp.php'; ?>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'c9e72953b4a9077f988c7f4fa779826e78e0bde9';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
 </head>
 <body>
 <header class="site-header">
